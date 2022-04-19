c9c88f20787b14598148676536e0666d3b72ed28
Abraham Vela <abvela09@mail.wou.edu>
2022-04-19 14:59:34 -0700

    added user side validation for email and a calender for date of birth., still need to update it to the db.

---
+++ sprint_3/ProjectDiscordStat/DiscordStat/DAL/Concrete/DiscordUserAndUserWebSiteInfoRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DiscordStats.DAL.Abstract;
+using DiscordStats.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscordStats.DAL.Concrete
+{
+    public class DiscordUserAndUserWebSiteInfoRepository : Repository<DiscordUserAndUserWebSiteInfo>, IDiscordUserAndUserWebSiteInfoRepository
+    {
+        public DiscordUserAndUserWebSiteInfoRepository(DiscordDataDbContext ctx) : base(ctx)
+        {
+        }
+
+        public IEnumerable<DiscordUserAndUserWebSiteInfo> GetDiscordUsers()
+        {
+            List<DiscordUserAndUserWebSiteInfo> discordusers = new List<DiscordUserAndUserWebSiteInfo>();
+            foreach (var s in _dbSet)
+            {
+                discordusers.Add(s);
+            }
+
+            return discordusers;
+
+        }
+    }
+}
