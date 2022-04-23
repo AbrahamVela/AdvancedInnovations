b1c731f10d6660239cf3b64043089b383ac4531d
Korbin Cardoza <kcardoza17@wou.edu>
2022-04-23 15:49:07 -0700

    Resolving errors

--- sprint_3/ProjectDiscordStat/DiscordStat/DAL/Abstract/IMessageInfoRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using DiscordStats.Models;
-
-namespace DiscordStats.DAL.Abstract
-{
-    public interface IMessageInfoRepository : IRepository<MessageInfo>
-    {
-    }
-}
--- sprint_3/ProjectDiscordStat/DiscordStat/ViewModel/ServerIdAndGameNameVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DiscordStats.ViewModel
-{
-    public class ServerIdAndGameNameVM
-    {
-        public string ServerId { get; set; }
-        public string GameName { get; set; }
-    }
-}
