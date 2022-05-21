add75e5c18701db8d8a68287eb08ca9948906ff9
Korbin Cardoza <kcardoza17@wou.edu>
2022-05-21 14:28:00 -0700

    Fixing

--- sprint_3/ProjectDiscordStat/DiscordStat/Controllers/StatsController.cs
+++ sprint_3/ProjectDiscordStat/DiscordStat/Controllers/StatsController.cs
@@ -105,13 +105,12 @@ namespace DiscordStats.Controllers
             else
                 return RedirectToAction("Account", "Account");
         }
-
-
-        [HttpGet]
-        public async Task<IActionResult> GetMessageInfoFromDatabase(string ServerId)
+        [HttpGet] 
+        public async Task<IActionResult> GetVoiceStatesFromDatabaseForGraphAndDownload(string formatWithServerId)
         {
+            var formatWithServerIdSplitted = formatWithServerId.Split(":");
             bool authenticated = false;
-            var usersInGuild = await _discord.GetCurrentGuildUsers(_configuration["API:BotToken"], ServerId);
+            var usersInGuild = await _discord.GetCurrentGuildUsers(_configuration["API:BotToken"], formatWithServerIdSplitted[1]);
             if (usersInGuild == null)
             {
                 return RedirectToAction("Account", "Account");
@@ -124,7 +123,41 @@ namespace DiscordStats.Controllers
             }
             if (authenticated)
             {
-                return Json(_messageInfoRepository.GetAll().Where(s => s.ServerId == ServerId).ToList());
+                var data = _voiceStateRepository.GetAll().Where(s => s.ServerId == formatWithServerIdSplitted[1]).ToList();
+                var result = new { dataFromDB = data, format = formatWithServerIdSplitted[0] };
+                return Json(result);
+            }
+            else
+                return RedirectToAction("Account", "Account");
+        }
+        [HttpGet]
+        public IActionResult GetMessageInfoFromDatabase(string ServerId)
+        {
+            var item = _messageInfoRepository.GetAll().Where(s => s.ServerId == ServerId).ToList();
+            return Json(item);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetMessageInfoFromDatabaseForGraphAndDownload(string formatWithServerId)
+        {
+            var formatWithServerIdSplitted = formatWithServerId.Split(":");
+
+            bool authenticated = false;
+            var usersInGuild = await _discord.GetCurrentGuildUsers(_configuration["API:BotToken"], formatWithServerIdSplitted[1]);
+            if (usersInGuild == null)
+            {
+                return RedirectToAction("Account", "Account");
+            }
+            var name = User.Claims.First(c => c.Type == ClaimTypes.Name).Value;
+            foreach (var u in usersInGuild)
+            {
+                if (u.user.UserName == name)
+                    authenticated = true;
+            }
+            if (authenticated)
+            {
+                var data = _messageInfoRepository.GetAll().Where(s => s.ServerId == formatWithServerIdSplitted[1]).ToList();
+                var result = new { dataFromDB = data, format = formatWithServerIdSplitted[0] };
+                return Json(result);
             }
             else
                 return RedirectToAction("Account", "Account");
@@ -153,11 +186,13 @@ namespace DiscordStats.Controllers
                 return RedirectToAction("Account", "Account");
         }
         [HttpGet]
-        public async Task<IActionResult> GetAllPresencesFromDatabase(string ServerId)
+        public async Task<IActionResult> GetAllPresencesFromDatabaseForGraphAndDownload(string formatWithServerId)
         {
 
+            var formatWithServerIdSplitted = formatWithServerId.Split(":");
+
             bool authenticated = false;
-            var usersInGuild = await _discord.GetCurrentGuildUsers(_configuration["API:BotToken"], ServerId);
+            var usersInGuild = await _discord.GetCurrentGuildUsers(_configuration["API:BotToken"], formatWithServerIdSplitted[1]);
             if (usersInGuild == null)
             {
                 return RedirectToAction("Account", "Account");
@@ -170,7 +205,9 @@ namespace DiscordStats.Controllers
             }
             if (authenticated)
             {
-                return Json(_presenceRepository.GetAll().Where(s => s.ServerId == ServerId).ToList());
+                var data = _presenceRepository.GetAll().Where(s => s.ServerId ==  formatWithServerIdSplitted[1]).ToList();
+                var result = new { dataFromDB = data, format = formatWithServerIdSplitted[0] };
+                return Json(result);
             }
             else
                 return RedirectToAction("Account", "Account");
@@ -178,11 +215,11 @@ namespace DiscordStats.Controllers
 
 
         [HttpGet]
-        public async Task<IActionResult> GetStatusesFromDatabase(string ServerId)
+        public async Task<IActionResult> GetStatusesFromDatabaseForGraphAndDownload(string formatWithDetailsServerId)
         {
-
+            var formatWithDetailsServerIdSplitted = formatWithDetailsServerId.Split(":");
             bool authenticated = false;
-            var usersInGuild = await _discord.GetCurrentGuildUsers(_configuration["API:BotToken"], ServerId);
+            var usersInGuild = await _discord.GetCurrentGuildUsers(_configuration["API:BotToken"], formatWithDetailsServerIdSplitted[1]);
             if (usersInGuild == null)
             {
                 return RedirectToAction("Account", "Account");
@@ -195,7 +232,9 @@ namespace DiscordStats.Controllers
             }
             if (authenticated)
             {
-                return Json(_statusRepository.GetAll().Where(s => s.ServerId == ServerId).ToList());
+                var data = _statusRepository.GetAll().Where(s => s.ServerId == formatWithDetailsServerIdSplitted[1]).ToList();
+                var result = new { dataFromDB = data, format = formatWithDetailsServerIdSplitted[0] };
+                return Json(result);
             }
             else
                 return RedirectToAction("Account", "Account");
@@ -217,6 +256,7 @@ namespace DiscordStats.Controllers
             }
             if (authenticated)
             {
+                var a = _presenceRepository.GetAll().Where(s => s.ServerId == ServerId && s.Name == GameName).ToList();
                 return Json(_presenceRepository.GetAll().Where(s => s.ServerId == ServerId && s.Name == GameName).ToList());
             }
             else
@@ -225,16 +265,33 @@ namespace DiscordStats.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymous]
-        public IActionResult GetPresenceDataFromDb()
+        public async Task<IActionResult> GetPresencesFromDatabaseForGraphAndDownload(string formatWithDetailsServerId, string GameName)
         {
-            _logger.LogInformation("GetPresenceDataFromDb");
-            List<Presence> presences = _presenceRepository.GetAll().ToList();
-            PresenceChartDataVM presenceChartDataVM = new();
-            var presencesNameAndCount = presenceChartDataVM.AllPresenceNameListAndCount(presences);
+            var formatWithDetailsServerIdSplitted = formatWithDetailsServerId.Split(":");
 
-            return Json(new { userPerGame = presencesNameAndCount });
+            bool authenticated = false;
+            var usersInGuild = await _discord.GetCurrentGuildUsers(_configuration["API:BotToken"], formatWithDetailsServerIdSplitted[1]);
+            if (usersInGuild == null)
+            {
+                return RedirectToAction("Account", "Account");
+            }
+            var name = User.Claims.First(c => c.Type == ClaimTypes.Name).Value;
+            foreach (var u in usersInGuild)
+            {
+                if (u.user.UserName == name)
+                    authenticated = true;
+            }
+            if (authenticated)
+            {
+                var data = _presenceRepository.GetAll().Where(s => s.ServerId == formatWithDetailsServerIdSplitted[1] && s.Name == GameName).ToList();
+                var result = new { dataFromDB = data, format = formatWithDetailsServerIdSplitted[0] };
+                return Json(result);
+            }
+            else
+                return RedirectToAction("Account", "Account");
+
         }
+
 
         [HttpGet]
         public async Task<IActionResult> GetServerMemberFromDatabaseWithDateForGraphAndDownload(string formatWithDetailsServerId, string startDate, string endDate)
@@ -295,5 +352,17 @@ namespace DiscordStats.Controllers
                 return RedirectToAction("Account", "Account");
         }
 
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult GetPresenceDataFromDb()
+        {
+            _logger.LogInformation("GetPresenceDataFromDb");
+            List<Presence> presences = _presenceRepository.GetAll().ToList();
+            PresenceChartDataVM presenceChartDataVM = new();
+            var presencesNameAndCount = presenceChartDataVM.AllPresenceNameListAndCount(presences);
+
+            return Json(new { userPerGame = presencesNameAndCount });
+        }
     }
 }
---
+++ sprint_3/ProjectDiscordStat/DiscordStat/js/ServerGrowth.js
@@ -0,0 +1,177 @@
+﻿$(document).ready(function () {
+    var data = 0;
+    ajaxServerMembersForActiveMemberInServer(data);
+
+    console.log("URL: " + '../Stats/GetServerMemberFromDatabase?serverid=' + detailsServerId);
+});
+
+function GetActiveMemberInServer(data) {
+    ajaxServerMembersForActiveMemberInServer(data);
+};
+
+function UpdateMessages() {
+    var data = 0;
+    ajaxServerMembersForActiveMemberInServer(data);
+};
+
+
+function ajaxServerMembersForActiveMemberInServer(data) {
+    var startDate = document.getElementById('start').value;
+    var endDate = document.getElementById('end').value;
+    let detailsServerId = $("#ServerId").attr('value');
+    let formatWithDetailsServerId = data + ":" + detailsServerId
+
+    $.ajax({
+        type: "GET",
+        url: '../Stats/GetServerMemberFromDatabaseWithDateForGraphAndDownload?formatWithDetailsServerId=' + formatWithDetailsServerId + '&' + 'startDate=' + startDate + '&' + 'endDate=' + endDate,
+        success: barGraphHourlyPresenceActivity,
+        error: handleError
+    });
+}
+
+
+var presenceActivityData = [];
+var tempPresenceActivityData = [];
+var presencesChart;
+
+
+function handleError(xhr, ajaxOptions, thrownError) {
+    console.log('ajax error: ' + xhr.status);
+}
+
+
+
+function barGraphHourlyPresenceActivity(data) {
+    if (presencesChart != null) {
+        presencesChart.destroy();
+    }
+    presenceActivityData = data
+    tempPresenceActivityData = data
+    graphingPresenceActivity(tempPresenceActivityData.dataFromDB, data.format)
+}
+
+
+function graphingPresenceActivity(data, format) {
+
+
+    $("#ServerGrowthAllTimeChart").empty();
+
+    var xValues = [];
+    var yValues = [];
+
+
+    for (var i = 0; i < data.length; i++) {
+
+        var datecheck = new Date(data[i].date).toLocaleDateString()
+                xValues[i] = datecheck;
+                yValues[i] = data[i].members; 
+    }
+    console.log(xValues);
+    console.log(yValues);
+
+    if (format != 0) {
+        var obj = {};
+        for (var i = 0; i < xValues.length; i++) {
+            obj[xValues[i]] = yValues[i];
+        };
+
+        downloadServerGrowth(data, obj, format);
+    }
+
+    else {
+        presencesChart = new Chart("ServerGrowthAllTimeChart", {
+            type: "line",
+            data: {
+                labels: xValues,
+                datasets: [
+                    {
+                        borderColor: "black",
+                        backgroundColor: "rgb(255, 99, 132)",
+                        data: yValues,
+                        ticks: {
+                            beginAtZero: false
+                        }
+                    }]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    title: {
+                        display: true,
+                        text: "Active Members in Server",
+                        padding: 10,
+                        color: 'black',
+                        font: {
+                            size: 25
+                        }
+                    },
+                },
+                scales: {
+                    y: {
+                        title: {
+                            display: true,
+                            text: 'Members',
+                            padding: 10,
+                            color: 'black',
+                            font: {
+                                size: 25
+                            }
+                        },
+                        ticks: {
+                            beginAtZero: false,
+                            precision: 0,
+                            color: 'black',
+                            font: {
+                                size: 20
+                            }
+                        }
+
+                    },
+                    x: {
+                        ticks: {
+                            precision: 0,
+                            color: 'white',
+                            font: {
+                                size: 16,
+                                family: 'Helvetica'
+                            }
+                        }
+                    }
+                },
+
+            }
+        })
+    }
+};
+
+function downloadServerGrowth(data, obj,  format) {
+    var zero = JSON.stringify(obj);
+
+    var element = document.createElement('a');
+    if (format == 1) {
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(zero));
+
+        element.setAttribute('download', "ActiveMembersInServer.json");
+        format = 0;
+        graphingPresenceActivity(data, format)
+    }
+    if (format == 2) {
+        var something = JSONToCSVConvertor(zero);
+
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(something));
+        element.setAttribute('download', "ActiveMembersInServer.csv");
+        format = 0;
+        graphingPresenceActivity(data, format)
+    }
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+
